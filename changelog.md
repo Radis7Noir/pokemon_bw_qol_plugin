23fa90b7a0a5be246e030579265099827dd9163a
pantin21 <trunks161@hotmail.fr>
2026-06-08 16:47:15 +0200

    everything v13

---
+++ changelog.md
@@ -0,0 +1,115 @@
+# Pokémon BW AP QoL Plugin
+
+Changelog
+
+## v12
+* Added a Repel re-use prompt (all credit goes to @ OnlySpaghettiCode)
+* Added two new YAML options: `bike_everywhere` and `remove_surf_bike_music`
+  * `bike_everywhere` allows the Bicycle to be used nearly everywhere, including inside buildings
+  * `remove_surf_bike_music` prevents the BGM to change when surfing or biking
+
+## v11
+* Removed the fanfare skips in the Player's Room to fully fix compatibility with the Starter Rando plugin
+
+## v10
+* Pokémon League and N's Castle
+  * Prevented a softlock when going back to N's Castle 1F after using the Delete 4 NPC
+  * Added a trigger to stop the player going back to the Champion's Room from N's Castle 1F
+  * Adjusted some flags to prevent visual issues when fighting Alder in the Champion's Room after using Delete 4
+* Nuvema Town
+  * Adjusted the script of the Player's Room to fix compatibility with the Starter Rando plugin
+* Field Moves
+  * Cut, Strength, Waterfall and Dive do not require teaching anymore to be used
+  * Removed the confirmation window before using a field move
+  * ???
+* Added an Easter Egg somewhere in Unova
+
+## v9
+* Striaton City
+  * Changed the walk animations to fast walk
+* Route 3 and Wellspring Cave
+  * Removed the mandatory double battle against the twins. Talking to them will initiate the fight if you have at least two Pokémon
+  * Moved the Cheren fight closer to Wellspring Cave, making it possible to go to Nacrene City before triggering it
+  * Removed the trigger that forces the player to finish the Wellspring Cave event before going to Nacrene City
+  * Removed the cutscene where Cheren explains how dark grass work. A permanent NPC takes his place and can be talked to to get the explanation
+  * Changed the walk animations to fast walk
+* Nacrene City and Museum
+  * Made the fight against N unskippable. Without the plugin, the fight can be missed if you complete Pinwheel Forest before entering Nacrene Museum
+  * Adjusted multiple flags to avoid characters like Lenora being on 2 different places on the map at the same time
+  * Changed the walk animations to fast walk
+* Pinwheel Forest
+  * Removed the trigger that forces the player to clear Pinwheel Forest before going to the Dragon Skull check. Without the plugin, it is only doable before clearing Nacrene Gym
+  * Fixed invisible Burgh in the last cutscene if you fight team Plasma before clearing Nacrene Gym
+  * Fixed the BGM being stuck to the team Plasma event if you clear Nacrene Gym after Pinwheel Forest
+  * Changed the walk animations to fast walk
+
+## v8
+* Fixed the Pokémon League Center Plasma grunt NPC not teleporting in front of the Throne Room
+
+## v7
+* Adjusted Delete 4
+  * Made the Plasma grunt in the Pokémon League Center teleport the player in front of the Throne Room instead of N's Castle 3F
+  * Added a Plasma grunt NPC in front of the Throne Room to warp back to the Pokémon League Center
+  * Adjusted the dialogs of the NPC guarding the Elite Four Entrance to mention the Delete 4 Plasma grunt NPC
+  * Talking to Ghetsis before entering the Throne Room is now optional
+  * Added a dialog prompt to skip the Throne Room cutscenes and fight the Legendary right away
+  * Catching the Legendary is not required anymore to progress the story
+  * Adding the Legendary to the party will not put it in first place anymore
+  * Changed the walk animations to fast walk
+* Adjusted some events at the beginning of the game
+  * Mom will now give the Town Map and the Running Shoes just after the XTransceiver
+  * Removed the Mom cutscene after obtaining the Pokédex
+  * Removed the Mom cutscene on Route 2
+  * Adjusted the cutscene in Juniper Lab to make Cheren and Bianca leave before the player
+
+## v6
+* Fixed a texture issue in Mistralton Gym
+* Fixed invisible Cheren on Route 4
+
+## v5
+* Fixed an issue with fanfare skips that prevented some BGM changes (like the Follow Me music getting stuck in Nacrene City)
+  * A small delay has been added to some item pickups because of that
+* Shortened Nuvema Town to Route 2 events
+  * Adjusted some of the dialogs
+  * Removed the cutscene in Bianca's house
+  * Removed the cutscene at the end of Route 1 and the XTransceiver call
+  * Removed the Accumula Pokémon Center cutscene
+  * Removed the XTransceiver call on Route 2
+  * Made Prof Juniper skip the catch tutorial and go back to Nuvema Town
+  * Cut some animations and dialogs in the Ghetsis cutscene of Accumula Town
+  * Changed the walk animations to fast walk
+
+## v4
+* Added optional gym warps to make gyms faster
+  * Taking the warps in Striaton Gym and Nacrene Gym will solve their puzzles
+* Removed the badge case cutscenes normally playing after defeating a Gym Leader
+* Added a dialog prompt to take a shortcut for Skyarrow Bridge
+* Shortened the Burgh events in Castelia City
+  * Removed the sequence on Prime Pier
+  * Changed the walk animations to fast walk
+* Shortened the Bianca events in Nimbasa City
+  * Changed the walk animations to fast walk
+  * Removed the trigger forcing the player to follow Bianca when attempting to go to Route 5 before visiting the Musical Theater
+  * Removed the Dress up tutorial and adjusted the dialog for it
+  * Removed the cutscene with Bianca's dad after going to the Musical Theater
+* Shortened N's event in Nimbasa City East
+  * Changed the walk animations to fast walk
+  * Removed the 3D ferry wheel cutscene
+* Shortened the events on Route 5 after defeating Elesa
+  * Changed the walk animations to fast walk
+  * Removed the 3D bridge cutscene and adjusted the dialog for it
+
+## v3
+* Added Instant Text
+  * Fixed the freeze when mashing buttons at the start of a trainer battle from the QoL script
+  * Fixed the empty text boxes issues from the QoL script
+* Added Delete 4
+  * The Plasma Grunt in Pokémon League Center can teleport the player in N's Castle as soon as you reach Pokémon League
+
+## v2
+* Switched to OTPP patches
+
+## v1
+* Removes HM cut-ins
+* Removes item pickup animations and enables skippable fanfare
+* Removes the catching tutorial on Route 1
---
+++ pokemon_bw_qol_plugin/__init__.py
@@ -0,0 +1,166 @@
+# Do not change anything about the imports unless you know what you do
+import pkgutil
+import orjson
+from typing import TYPE_CHECKING
+from types import ModuleType
+
+if TYPE_CHECKING:
+    try:
+        from worlds.pokemon_bw.ndspy.rom import NintendoDSRom
+        from worlds.pokemon_bw.rom import PokemonBWPatch
+    except:
+        pass
+
+
+# This has to exactly be named "Plugin" and needs to have a "name" field and a "patch" method
+class Plugin:
+
+    # name is unused atm, but better set it to the same as in archipelago.json
+    name = "Pokemon BW QoL Plugin"
+
+    # "rom" is the, well, ROM, but unpacked instead of raw bytes
+    # "bw_patch_instance" is the class that is responsible for the standard patches
+    # "files_dump" is used for dumping any file that was patched into a zip folder for debugging (enabled via host.yaml setting)
+    # "plugins" is a list of all other plugin modules, in case you have to account for certain other plugins being loaded
+    @staticmethod
+    def patch(rom: "NintendoDSRom", bw_patch_instance: "PokemonBWPatch", files_dump: dict[str, bytes | bytearray],
+              plugins: list[tuple[str, ModuleType]]):
+        from worlds.pokemon_bw.ndspy.code import saveOverlayTable, codeCompression
+        from worlds.pokemon_bw.ndspy.narc import NARC
+        from worlds.pokemon_bw.patch import otpp
+
+        slot_data = orjson.loads(bw_patch_instance.files.get("slot_data.json", b'{}'))
+        options: dict[str, Any] = slot_data.get("options", {})
+        plugin_options: dict[str, Any] = options.get("plugin_options", {})
+        qol_options: dict[str, Any] = plugin_options.get("qol", {})
+
+# Instant Text
+        if qol_options.get("instant_text", False):
+            arm9 = bytearray(codeCompression.decompress(rom.arm9))
+            if rom.name[:9] == b'POKEMON\x20W':
+                arm9[0x18fda:0x18fe0] = b'\x00\x28\xc0\x46\xc0\x46'
+                arm9[0x19090:0x19098] = b'\x00\x48\x00\x47\x39\x05\x0a\x02'
+                extra_code = pkgutil.get_data(__name__, "files/w_insta_text_append.bin")
+                arm9[0x9c538:0x9c538+len(extra_code)] = extra_code
+            else:
+                arm9[0x18fc0:0x18fc4] = b'\xc0\x46\xc0\x46'
+                arm9[0x19074:0x1907c] = b'\x00\x48\x00\x47\x21\x05\x0a\x02'
+                extra_code = pkgutil.get_data(__name__, "files/b_insta_text_append.bin")
+                arm9[0x9c520:0x9c520+len(extra_code)] = extra_code
+            arm9 = bytearray(codeCompression.compress(arm9, True))
+            arm9[0xfc4:0xfc7] = (len(arm9) + 0x4000).to_bytes(3, "little")
+            rom.arm9 = bytes(arm9)
+            files_dump["arm9"] = rom.arm9
+
+# Faster Story
+        if qol_options.get("faster_story", False):
+            narc = NARC(rom.getFileByName("a/0/0/3"))
+            for i in [82, 349, 353, 361, 428, 429, 435, 436]:
+                loaded_file = pkgutil.get_data(__name__, f"files/a003/a003_{i:03d}")
+                narc.files[i] = otpp.patch(narc.files[i], loaded_file)
+                files_dump[f"a003/{i}"] = narc.files[i]
+            rom.setFileByName("a/0/0/3", narc.save())
+
+            narc = NARC(rom.getFileByName("a/0/5/7"))
+            for i in [12, 14, 18, 20, 32, 34, 36, 38, 58, 62, 124, 126, 128, 154, 194, 216, 228, 242, 308, 310, 634, 638, 642, 648, 658, 778, 780, 782, 792, 794]:
+                loaded_file = pkgutil.get_data(__name__, f"files/a057/a057_{i:03d}")
+                narc.files[i] = otpp.patch(narc.files[i], loaded_file)
+                files_dump[f"a057/{i}"] = narc.files[i]
+            rom.setFileByName("a/0/5/7", narc.save())
+
+            narc = NARC(rom.getFileByName("a/1/2/5"))
+            for i in [16, 28, 62, 154, 155, 321]:
+                loaded_file = pkgutil.get_data(__name__, f"files/a125/a125_{i:03d}")
+                narc.files[i] = otpp.patch(narc.files[i], loaded_file)
+                files_dump[f"a125/{i}"] = narc.files[i]
+            rom.setFileByName("a/1/2/5", narc.save())
+
+# Field Moves
+        narc1 = NARC(rom.getFileByName("a/0/0/3"))
+        narc2 = NARC(rom.getFileByName("a/0/5/7"))
+        match qol_options.get("hm_use", False):
+            case "fast":
+                suffix = "fast"
+            case "fastest":
+                suffix = "fastest"
+            case _:
+                suffix = "rock_smash"
+        loaded_file = pkgutil.get_data(__name__, f"files/a003/a003_280_{suffix}")
+        narc1.files[280] = otpp.patch(narc1.files[280], loaded_file)
+        files_dump["a003/280"] = narc1.files[280]
+        rom.setFileByName("a/0/0/3", narc1.save())
+        loaded_file = pkgutil.get_data(__name__, f"files/a057/a057_867_{suffix}")
+        narc2.files[867] = otpp.patch(narc2.files[867], loaded_file)
+        files_dump["a057/867"] = narc2.files[867]
+        rom.setFileByName("a/0/5/7", narc2.save())
+
+# Guaranteed Fishing
+        if qol_options.get("guaranteed_fishing", False):
+            narc = NARC(rom.getFileByName("a/1/2/6"))
+            for i in [0, 1, 2, 6, 49, 53, 58, 69, 71, 72, 73, 75, 77, 79, 80, 81, 82, 84, 93, 94, 98, 101, 103, 104, 105, 108, 109, 110, 111]:
+                loaded_file = pkgutil.get_data(__name__, f"files/a126/a126_{i:03d}")
+                narc.files[i] = otpp.patch(narc.files[i], loaded_file)
+                files_dump[f"a126/{i}"] = narc.files[i]
+            rom.setFileByName("a/1/2/6", narc.save())
+
+# Bike Everywhere / Remove Surf & Bike Music
+        bike_everywhere = qol_options.get("bike_everywhere", False)
+        remove_surf_bike_music = qol_options.get("remove_surf_bike_music", False)
+        if bike_everywhere or remove_surf_bike_music:
+            narc = NARC(rom.getFileByName("a/0/1/2"))
+            if bike_everywhere:
+                loaded_file = pkgutil.get_data(__name__, "files/a012/a012_000_bike")
+                narc.files[0] = otpp.patch(narc.files[0], loaded_file)
+
+            if remove_surf_bike_music:
+                loaded_file = pkgutil.get_data(__name__, "files/a012/a012_000_music")
+                narc.files[0] = otpp.patch(narc.files[0], loaded_file)
+            files_dump["a012/0"] = narc.files[0]
+            rom.setFileByName("a/0/1/2", narc.save())
+
+# Gym Warps
+        if qol_options.get("gym_warps", False):
+            narc = NARC(rom.getFileByName("a/0/0/8"))
+            for i in [515, 518, 519, 520, 524, 525, 527, 538, 548, 552, 564, 565, 595, 606, 615]:
+                loaded_file = pkgutil.get_data(__name__, f"files/a008/a008_{i:03d}")
+                narc.files[i] = otpp.patch(narc.files[i], loaded_file)
+                files_dump[f"a008/{i}"] = narc.files[i]
+            rom.setFileByName("a/0/0/8", narc.save())
+
+            narc = NARC(rom.getFileByName("a/1/2/5"))
+            for i in [7, 18, 19, 29, 63, 97, 98, 108, 114, 121]:
+                loaded_file = pkgutil.get_data(__name__, f"files/a125/a125_{i:03d}")
+                narc.files[i] = otpp.patch(narc.files[i], loaded_file)
+                files_dump[f"a125/{i}"] = narc.files[i]
+            rom.setFileByName("a/1/2/5", narc.save())
+
+# No Pickup Animation & Fanfare Skip
+        if qol_options.get("remove_pickup_animation", False):
+            narc = NARC(rom.getFileByName("a/0/5/7"))
+            for i in [862, 864]:
+                loaded_file = pkgutil.get_data(__name__, f"files/a057/a057_{i:03d}")  # not hardcoded _862
+                narc.files[i] = otpp.patch(narc.files[i], loaded_file)
+                files_dump[f"a057/{i}"] = narc.files[i]
+            rom.setFileByName("a/0/5/7", narc.save())
+
+# Always On (Repel Prompt, Shortcuts, Delete 4)
+        narc = NARC(rom.getFileByName("a/0/0/3"))
+        for i in [82, 141, 263, 267, 268, 300]:
+            loaded_file = pkgutil.get_data(__name__, f"files/a003/a003_{i:03d}")
+            narc.files[i] = otpp.patch(narc.files[i], loaded_file)
+            files_dump[f"a003/{i}"] = narc.files[i]
+        rom.setFileByName("a/0/0/3", narc.save())
+
+        narc = NARC(rom.getFileByName("a/0/5/7"))
+        for i in [272, 292, 502, 504, 528, 554, 556, 872]:
+            loaded_file = pkgutil.get_data(__name__, f"files/a057/a057_{i:03d}")
+            narc.files[i] = otpp.patch(narc.files[i], loaded_file)
+            files_dump[f"a057/{i}"] = narc.files[i]
+        rom.setFileByName("a/0/5/7", narc.save())
+
+        narc = NARC(rom.getFileByName("a/1/2/5"))
+        for i in [251, 252, 264, 277]:
+            loaded_file = pkgutil.get_data(__name__, f"files/a125/a125_{i:03d}")
+            narc.files[i] = otpp.patch(narc.files[i], loaded_file)
+            files_dump[f"a125/{i}"] = narc.files[i]
+        rom.setFileByName("a/1/2/5", narc.save())
